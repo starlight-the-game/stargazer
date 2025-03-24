0e120eb51430c197f775153f5f57c6cd30cbf3cc
Tien Dat Pham <dat20036@gmail.com>
2025-03-24 09:45:01 +0700

    feat: properly introduce Prisma database.
    Yes, I do know the dependency is outdated.

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use aide::{axum::ApiRouter, openapi::OpenApi};
-use axum::{routing::get, Extension};
+use axum::{Extension, routing::get};
 use openapi::api_document::docs_routes;
 use routes::auth::configure_auth_routes;
 use std::sync::Arc;
 use tokio::net::TcpListener;
 use tower_http::trace::TraceLayer;
-use tracing::{debug, error, Level};
+use tracing::{Level, debug, error};
 
 mod openapi;
 #[allow(dead_code, unused_imports)]
@@ -13,6 +13,7 @@ mod prisma;
 mod routes;
 
 use openapi::api_document::api_docs;
+use crate::prisma::PrismaClient;
 
 #[tokio::main]
 async fn main() {
@@ -26,8 +27,11 @@ async fn main() {
         .with_max_level(Level::DEBUG)
         .init();
 
+    let prisma_client = Arc::new(prisma::new_client()
+        .await
+        .expect("Failed to create Prisma client")
+    );
     let mut api = OpenApi::default();
-
     let listener = TcpListener::bind("0.0.0.0:5000").await.unwrap();
 
     let app = ApiRouter::new()
@@ -36,6 +40,7 @@ async fn main() {
         .nest_api_service("/api", configure_auth_routes())
         .finish_api_with(&mut api, api_docs)
         .layer(Extension(Arc::new(api)))
+        .layer(Extension(prisma_client))
         .layer(TraceLayer::new_for_http());
 
     debug!("The application is on http://127.0.0.1:5000/");
